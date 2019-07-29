40f4b773359e97ad97bdbbe51dcef8e6fa62e38e
RN804 <32243415+roxey804@users.noreply.github.com>
2019-07-29 18:00:04 +0100

    removed centering from navbar fix

--- js/script.js
+++ js/script.js
@@ -17,3 +17,13 @@ $("#hiddentext").click(function(){
     $("#hiddentext").slideUp();
   });
     });
+
+
+    function myFunction() {
+        var x = document.getElementById("centered_nav");
+        if (x.className === "rc_nav") {
+            x.className += " responsive";
+        } else {
+            x.className = "rc_nav";
+        }
+    }
